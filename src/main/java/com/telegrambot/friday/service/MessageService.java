3dd085b621f1733e35dd7c81e11918bad16fb11d
General Admiral Nuradil <a.nuradil.m@gmail.com>
2023-03-24 19:27:26 +0600

    add StateHandler (without handle botstate)

--- src/main/java/com/telegrambot/friday/service/MessageService.java
+++
@@ -1,8 +0,0 @@
-package com.telegrambot.friday.service;
-
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-
-public interface MessageService {
-    SendMessage send(long chatId, String message);
-    SendMessage weatherInfo(long chatId);
-}
